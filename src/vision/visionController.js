84ed91b7bedf1eb8d6ba4d8db363b3d68ae267fc
Faizal <faizalf@maxyfi.com>
2025-05-16 18:21:03 +0530

    feature added - JSON output

--- src/vision/visionController.js
+++ src/vision/visionController.js
@@ -1,6 +1,7 @@
 import { ChatOllama } from "@langchain/ollama";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 import {
   HumanMessage,
   ChatMessage,
@@ -10,6 +11,7 @@ import c from "chalk";
 import fs from "fs";
 import { pdfToPng } from "pdf-to-png-converter";
 import dotenv from "dotenv";
+import { response } from "express";
 dotenv.config();
 
 const model = [
@@ -19,8 +21,6 @@ const model = [
   "moondream", // 3
   "hf.co/unsloth/Qwen2.5-VL-7B-Instruct-GGUF:IQ2_M", // 4
 ];
-
-console.log("\n");
 
 const localModel_ls = () => {
   const convertPdfToPng = async () => {
@@ -133,29 +133,62 @@ const cloudModel_ls = async () => {
 };
 
 const cloudModel = async () => {
-  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemma-3-27b-it" });
+  try {
+    const genAI = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 
-  const base64Image = fs
-    .readFileSync("src/vision/images/eleos-invoice-1.png")
-    .toString("base64");
-  const imagePart = {
-    inlineData: {
-      data: base64Image,
-      mimeType: "image/png",
-    },
-  };
+    // PDF → PNG
+    const convertPdfToPng = async () => {
+      const pdfFilePath = "src/vision/images/eleos_invoices.pdf";
 
-  const prompt = "what is this?";
+      const pngPages = await pdfToPng(pdfFilePath, {
+        viewportScale: 2.0,
+        verbosityLevel: 0,
+        outputFolder: "./output",
+        outputFileMaskFunc: (pageNumber) => `page_${pageNumber}.png`,
+      });
 
-  const resultStream = await model.generateContentStream([prompt, imagePart]);
+      console.log("Conversion complete: ", pngPages);
+    };
 
-  for await (const chunk of resultStream.stream) {
-    process.stdout.write(chunk.text());
+    // PNG → Base64
+    function encodeImageToBase64(filePath) {
+      const imageBuffer = fs.readFileSync(filePath);
+      return imageBuffer.toString("base64");
+    }
+
+    const imagePath = "src/vision/images/eleos-invoice-1.png";
+    const base64Image = encodeImageToBase64(imagePath);
+    const imagePart = {
+      inlineData: {
+        data: base64Image,
+        mimeType: "image/png",
+      },
+    };
+
+    const system =
+      "Only respond in single object. Your output will be converted to CSV, so maintain consistent keys across responses. identify handwritten correction (strikes) to the original values, and apply those adjustments.";
+
+    const prompt = "give all the details including the corrections";
+
+    let contents = [prompt];
+
+    const result = await genAI.models.generateContent({
+      model: "gemini-2.0-flash",
+      // model: "gemma-3-27b-it",
+      config: {
+        systemInstruction: system,
+        responseMimeType: "application/json",
+      },
+      contents: [prompt, imagePart],
+    });
+
+    // for await (const chunk of result) {
+    //   process.stdout.write(chunk.text);
+    // }
+    console.log(`\n${result.text}\n`);
+  } catch (error) {
+    console.error("Error generating content:", error);
   }
-
-  // const result = await model.generateContent([prompt, imagePart]);
-  // console.log(result.response.text());
 };
 
 cloudModel();
